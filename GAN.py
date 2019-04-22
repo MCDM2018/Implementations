643bd9da505067e478f44b48ac6ee8e8d0e959aa
MCDM2018 <45657024+MCDM2018@users.noreply.github.com>
2019-04-22 16:35:29 +0900

    Add files via upload
    It is an implementation for the paper named by 'Is Generator Conditioning Causally related to GAN performance'

---
+++ GAN.py
@@ -0,0 +1,331 @@
+import utils, torch, time, os, pickle
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from dataloader import dataloader
+import kernel
+import torchvision
+from torch.autograd import grad,Variable,gradcheck
+from itertools import product
+from torch._six import container_abcs
+import xlsxwriter
+
+
+# class jacobian(nn.Module):
+#     def __init__(self,generator,latent_dim,batch_size):
+#         super(jacobian,self).__init__()
+#         self.generator=generator
+#         self.batch_size=batch_size
+#         self.latent_dim=latent_dim
+#
+#     def forward(self, input):
+#         input=Variable(input,requires_grad=True)
+#         G_imgs = self.generator(input)
+#         G_imgs = G_imgs.view(self.batch_size, -1)
+#         jacobi = torch.zeros(self.batch_size, G_imgs.size(1), self.latent_dim)
+#
+#
+#         for p_idx in range(G_imgs.size(1)):
+#             target_tensor = Variable((torch.zeros(self.batch_size, G_imgs.size(1))).cuda(),requires_grad=True)
+#             target_tensor[:, p_idx] = 1
+#             G_imgs.backward(target_tensor, retain_graph=True,create_graph=True)
+#             jacobi[:, p_idx, :]=input.grad.data
+#
+#         return jacobi
+#
+
+class generator(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    def __init__(self, input_dim=100, output_dim=1, input_size=32):
+        super(generator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_size = input_size
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.input_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 128 * (self.input_size // 4) * (self.input_size // 4)),
+            nn.BatchNorm1d(128 * (self.input_size // 4) * (self.input_size // 4)),
+            nn.ReLU(),
+        )
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, self.output_dim, 4, 2, 1),
+            nn.Tanh(),
+        )
+        utils.initialize_weights(self)
+
+    def forward(self, input):
+        x = self.fc(input)
+        x = x.view(-1, 128, (self.input_size // 4), (self.input_size // 4))
+        x = self.deconv(x)
+
+        return x
+
+class discriminator(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
+    def __init__(self, input_dim=1, output_dim=1, input_size=32):
+        super(discriminator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_size = input_size
+        self.cov=kernel.Covariance(False)
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.input_dim, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(128 * (self.input_size // 4) * (self.input_size // 4), 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, self.output_dim),
+            nn.Sigmoid(),
+        )
+        utils.initialize_weights(self)
+
+    def forward(self, input):
+        x = self.conv(input)
+        x = x.view(-1, 128 * (self.input_size // 4) * (self.input_size // 4))
+        x = self.fc(x)
+
+        return x
+
+class GAN(object):
+    def __init__(self, args):
+        # parameters
+        self.epoch = args.epoch
+        self.sample_num = 100
+        self.batch_size = args.batch_size
+        self.save_dir = args.save_dir
+        self.result_dir = args.result_dir
+        self.dataset = args.dataset
+        self.log_dir = args.log_dir
+        self.gpu_mode = args.gpu_mode
+        self.model_name = args.gan_type
+        self.input_size = args.input_size
+        self.reg_lam=args.reg_lam
+        self.z_dim = 100
+        self.eig_min=args.eig_min
+        self.eig_max=args.eig_max
+        self.eps=args.eps
+        # load dataset
+        self.data_loader = dataloader(self.dataset, self.input_size, self.batch_size)
+        data = self.data_loader.__iter__().__next__()[0]
+
+        # networks init
+        self.G = generator(input_dim=self.z_dim, output_dim=data.shape[1], input_size=self.input_size)
+        self.D = discriminator(input_dim=data.shape[1], output_dim=1, input_size=self.input_size)
+        self.G_optimizer = optim.Adam(self.G.parameters(), lr=args.lrG, betas=(args.beta1, args.beta2))
+        self.D_optimizer = optim.Adam(self.D.parameters(), lr=args.lrD, betas=(args.beta1, args.beta2))
+
+        # self.jaco=jacobian(self.G,self.z_dim,self.batch_size)
+
+        if self.gpu_mode:
+            self.G.cuda()
+            self.D.cuda()
+            self.BCE_loss = nn.BCELoss().cuda()
+        else:
+            self.BCE_loss = nn.BCELoss()
+
+        print('---------- Networks architecture -------------')
+        utils.print_network(self.G)
+        utils.print_network(self.D)
+        print('-----------------------------------------------')
+
+
+        # fixed noise
+        self.sample_z_ = torch.rand((self.batch_size, self.z_dim))
+        if self.gpu_mode:
+            self.sample_z_ = self.sample_z_.cuda()
+
+
+    def train(self):
+        self.train_hist = {}
+        self.train_hist['D_loss'] = []
+        self.train_hist['G_loss'] = []
+        self.train_hist['per_epoch_time'] = []
+        self.train_hist['total_time'] = []
+
+        self.y_real_, self.y_fake_ = torch.ones(self.batch_size, 1), torch.zeros(self.batch_size, 1)
+        if self.gpu_mode:
+            self.y_real_, self.y_fake_ = self.y_real_.cuda(), self.y_fake_.cuda()
+
+        self.D.train()
+        print('training start!!')
+        start_time = time.time()
+        Q_stack=[]
+        for epoch in range(self.epoch):
+            self.G.train()
+            epoch_start_time = time.time()
+
+            for iter, (x_, _) in enumerate(self.data_loader):
+
+                if iter == self.data_loader.dataset.__len__() // self.batch_size:
+                    break
+
+                z_ = torch.rand((self.batch_size, self.z_dim))
+
+                if self.gpu_mode:
+                    x_, z_ = x_.cuda(), z_.cuda()
+
+                # update D network
+                self.D_optimizer.zero_grad()
+
+                D_real = self.D(x_)
+                D_real_loss = self.BCE_loss(D_real, self.y_real_)
+
+                G_ = self.G(z_)
+                D_fake = self.D(G_)
+                D_fake_loss = self.BCE_loss(D_fake, self.y_fake_)
+
+                D_loss = D_real_loss + D_fake_loss
+                self.train_hist['D_loss'].append(D_loss.item())
+
+                D_loss.backward()
+                self.D_optimizer.step()
+
+                # update G network
+
+                self.G_optimizer.zero_grad()
+                G_ = self.G(z_)
+                D_fake = self.D(G_)
+
+                pertubation_del=(torch.randn(self.batch_size, self.z_dim)).cuda()
+                eps=self.eps
+                pertu_length=torch.norm(pertubation_del, dim=1, keepdim=True)
+                pertubation_del = (pertubation_del / pertu_length) * eps
+                z_prime = z_ + pertubation_del
+                pertube_images=self.G(z_)-self.G(z_prime)
+                pertube_latent_var = z_ - z_prime
+                Q = torch.norm(pertube_images.view(self.batch_size, -1), dim=1) / torch.norm(pertube_latent_var.view(self.batch_size, -1), dim=1)
+                print(Q)
+
+
+                L_max = 0.0
+                L_min = 0.0
+                count_max=0
+                count_min=0
+
+                for i in range(self.batch_size):
+                    if Q[i] > self.eig_max:
+                        L_max += (Q[i] - self.eig_max) ** 2
+                        count_max+=1
+                    if Q[i] < self.eig_min:
+                        L_min += (Q[i] - self.eig_min) ** 2
+                        count_min+=1
+                L=L_max+L_min
+
+                G_loss = self.BCE_loss(D_fake, self.y_real_)
+
+                self.train_hist['G_loss'].append(G_loss.item())
+
+                G_loss.backward()
+
+                self.G_optimizer.step()
+
+
+                if ((iter + 1) % 100) == 0:
+                    print("Epoch: [%2d] [%4d/%4d] D_loss: %.8f, G_loss: %.8f" %
+                          ((epoch + 1), (iter + 1), self.data_loader.dataset.__len__() // self.batch_size, D_loss.item(), G_loss.item()))
+                    print(L)
+                    print(count_max)
+                    print(count_min)
+
+            self.train_hist['per_epoch_time'].append(time.time() - epoch_start_time)
+            with torch.no_grad():
+                self.gen_mode(4,epoch)
+                self.visualize_results((epoch+1))
+
+        self.train_hist['total_time'].append(time.time() - start_time)
+        print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (np.mean(self.train_hist['per_epoch_time']),
+              self.epoch, self.train_hist['total_time'][0]))
+        print("Training finish!... save training results")
+
+        self.save()
+        utils.generate_animation(self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name,
+                                 self.epoch)
+        utils.loss_plot(self.train_hist, os.path.join(self.save_dir, self.dataset, self.model_name), self.model_name)
+
+    def visualize_results(self, epoch, fix=True):
+        self.G.eval()
+
+        if not os.path.exists(self.result_dir + '/' + self.dataset + '/' + self.model_name):
+            os.makedirs(self.result_dir + '/' + self.dataset + '/' + self.model_name)
+
+        tot_num_samples = min(self.sample_num, self.batch_size)
+        image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
+
+        if fix:
+            """ fixed noise """
+            samples = self.G(self.sample_z_)
+        else:
+            """ random noise """
+            sample_z_ = torch.rand((self.batch_size, self.z_dim))
+            if self.gpu_mode:
+                sample_z_ = sample_z_.cuda()
+
+            samples = self.G(sample_z_)
+
+        if self.gpu_mode:
+            samples = samples.cpu().data.numpy().transpose(0, 2, 3, 1)
+        else:
+            samples = samples.data.numpy().transpose(0, 2, 3, 1)
+
+        samples = (samples + 1) / 2
+        utils.save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
+                          self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name + '_epoch%03d' % epoch + '.png')
+
+    def save(self):
+        save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        torch.save(self.G.state_dict(), os.path.join(save_dir, self.model_name + '_G.pkl'))
+        torch.save(self.D.state_dict(), os.path.join(save_dir, self.model_name + '_D.pkl'))
+
+        with open(os.path.join(save_dir, self.model_name + '_history.pkl'), 'wb') as f:
+            pickle.dump(self.train_hist, f)
+
+    def load(self):
+        save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
+
+        self.G.load_state_dict(torch.load(os.path.join('./'+save_dir, self.model_name + '_G.pkl')))
+        self.D.load_state_dict(torch.load(os.path.join('./'+save_dir, self.model_name + '_D.pkl')))
+    def gen_mode(self,iteration_numb,epoch):
+        self.G.eval()
+        if not os.path.exists(self.result_dir + '/' + self.dataset + '/' + self.model_name):
+            os.makedirs(self.result_dir + '/' + self.dataset + '/' + self.model_name)
+
+        tot_num_samples = min(self.sample_num, self.batch_size)
+        image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
+
+        # if self.gpu_mode:
+        #     samples = samples.cpu().data.numpy().transpose(0, 2, 3, 1)
+        # # else:
+        # samples = samples.data.numpy().transpose(0, 2, 3, 1)
+        # samples =torch.Tensor(samples)
+        # samples= samples.cuda()
+        for iter_idx in range(iteration_numb):
+            sample_z_ = torch.rand((self.batch_size, self.z_dim))
+            if self.gpu_mode:
+                sample_z_ = sample_z_.cuda()
+
+            samples = self.G(sample_z_)
+
+            for batch_idx,img in enumerate(samples):
+
+                img_root='./'+self.result_dir+'/'+'FID_set'+'/'+str(epoch)+self.dataset+'/'
+                img_path=img_root+str(iter_idx)+str(batch_idx)+'.png'
+                if not os.path.exists(img_root):
+                    os.makedirs(img_root)
+                torchvision.utils.save_image(img,img_path)
